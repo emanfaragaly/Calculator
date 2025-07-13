c8d174be00ae37c177305efcef2fec351324d39c
emanfaragaly <emanfaragaly@gmail.com>
2025-07-13 23:04:25 +0300

    details

--- js/app.js
+++ js/app.js
@@ -49,6 +49,7 @@ window.addEventListener("load", () => {
 // theme calculator
 
 ligth.addEventListener("click", () => {
+  result.focus();
   result.style.cssText = "color: black;";
   number.forEach((number) => {
     number.style.cssText = "color:rgb(255, 255, 255)";
@@ -59,12 +60,14 @@ ligth.addEventListener("click", () => {
   calc.style.cssText =
     " background-color:rgba(250, 250, 237, 0.3); box-shadow: 0 4px 8px #53aeeb;";
   equal.style.cssText = "background-color:rgb(83, 174, 235);";
+  
 });
 
 dark.addEventListener("click", () => {
+  result.focus();
   result.style.cssText = "color: white;";
   number.forEach((number) => {
-    number.style.cssText = "color:#53aeeb;";
+    number.style.color = "#53aeeb;";
   });
 
   operation.forEach((op) => {
@@ -73,5 +76,5 @@ dark.addEventListener("click", () => {
 
   calc.style.cssText =
     " background-color: #16160c; box-shadow: 0 4px 8px #394044;";
-  equal.style.cssText = "background-color:rgb(83, 174, 235);";
+  equal.style.backgroundColor = "rgb(83, 174, 235);";
 });
